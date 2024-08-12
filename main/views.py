8500bdaec170b91d11b9c083c0aab294675895b8
Konstantin Ilyushin <radist1981@mail.ru>
2024-08-12 14:27:30 +0300

    create index.html, paths, add css file, logo and favicon

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "title": "Главная страница",
+        "content": "Главная страница приложения"
+    }
+    return render(request, "main/index.html", context)
+
+def edit(request):
+    context = {
+        "title": "Редактирование",
+        "content": "Страница редактирования БД"
+    }
+    return render(request, "main/index.html", context)
+
+def send(request):
+    context = {
+        "title": "Отправка",
+        "content": "Страница отправки JSON"
+    }
+    return render(request, "main/index.html", context)
