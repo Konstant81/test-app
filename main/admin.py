22814bd7e1e6bf6261f14ecc5465244c2c08d19d
Konstantin Ilyushin <radist1981@mail.ru>
2024-08-15 17:06:25 +0300

    create and register client and order models, make migrations

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Client, Order
 
-# Register your models here.
+
+admin.site.register(Client)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['client', 'order_date', 'amount']
+    list_display = ['id', 'client', 'order_date', 'amount']
+    
--- main/models.py
+++ main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    fullname = models.CharField(max_length=100, null=False, verbose_name="имя_фамилия")
+
+    class Meta:
+        db_table = "client" # Имя таблицы в БД
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return str(self.fullname)
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING, verbose_name='Клиент')
+    order_date = models.DateField(verbose_name="дата заказа")
+    amount = models.IntegerField(null=False, verbose_name="сумма заказа")
+
+    class Meta:
+        db_table = "order"  # Имя таблицы в БД
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        # выполнение по умолчанию сортировки по убыванию даты заказов
+        ordering = ['-order_date']
+        # индексируем поле даты заказа для ускорения сортировки
+        indexes = [
+            models.Index(fields=['-order_date']),
+        ]
+
+    def __str__(self):
+        return str(self.id)
+    
