f1b0b35ff8e96f238e4121d5e4a7a68e7dffbf2a
Konstantin Ilyushin <radist1981@mail.ru>
2024-08-24 17:13:16 +0300

    make entrypiont for json response

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from datetime import date
+import base64
 from django.core import serializers
-from django.http import JsonResponse
-from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView, DeleteView, UpdateView
 from main.forms import AddOrderForm, EditdOrderForm
 from main.models import Order
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -35,12 +36,24 @@ def index(request):
                         GROUP BY "order"."order_date"''',
             [dates[0], dates[-1]],) if dates else False
     )
+    week_total = (
+        Order.objects.raw(
+            '''SELECT "order"."id", "order"."order_date",
+                        REPLACE(GROUP_CONCAT(DISTINCT "client"."fullname"), ',', '; ') AS clients,
+                        SUM("order"."amount") AS sum_amount
+                        FROM "order"
+                        JOIN "client" ON "order"."client_id" = "client"."id" 
+                        WHERE "order"."order_date" BETWEEN %s AND %s''',
+            [dates[0], dates[-1]],) if dates else False
+    )
+    amount_total, client_week = week_total[0].sum_amount if week_total else None, week_total[0].clients if week_total else None
 
     context = {
         "title": "Главная страница",
         "content": "Главная страница приложения",
         "week_orders": sum_amount,
-        "current_week": cur_week,
+        "amount_total": amount_total,
+        "client_week": client_week,
     }
     return render(request, "main/index.html", context)
 
@@ -55,15 +68,16 @@ def order(request):
     return render(request, "main/orders.html", context)
 
 
-@login_required
-def send(request):
-    context = {
-        "title": "Отправка",
-        "content": "Страница отправки JSON",
-    }
-    data = serializers.serialize("json", Order.objects.all())
-    return JsonResponse(data, safe=False)
-    # return render(request, "main/send.html", context)
+def get_json(request):
+    auth_user = "user1:1234"
+    auth_bytes = auth_user.encode("ascii")
+    b64_bytes = base64.b64encode(auth_bytes)
+    user_b64 = b64_bytes.decode("ascii")
+    auth_template = f'Basic {user_b64}'
+    if request.headers["Authorization"] == auth_template:
+        data = serializers.serialize("json", Order.objects.all())
+        return JsonResponse(data, safe=False)
+    return HttpResponse('Unauthorized', status=401)
 
 
 class AddOrder(CreateView):
